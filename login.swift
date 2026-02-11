a067dd92b5e8c6f69ef32b1d7939e82e220a226f
Prashant Borade Devops <prashantborade.devops@gmail.com>
2026-02-11 19:33:55 +0530

    added salary part

--- login.swift
+++ login.swift
@@ -1,7 +1,12 @@
 class Person {
-   var name: String 
-   init(name: String) {
+   var name: String
+   var ag: Int
+   var salary: Double 
+ 
+   init(name: String, age:Int, salary: Double) {
    self.name = name
+   self.age = age
+   self.salary = salary
   }
 }
 
